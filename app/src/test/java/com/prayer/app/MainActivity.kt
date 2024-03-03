7cba115ef28d2c5050e604f1789cd278e67f4885
LeenAlshehri2003 <miss.leen.2704@gmail.com>
2024-03-03 11:26:32 +0300

    Initial commit

---
+++ app/src/test/java/com/prayer/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.prayer.app
+
+import com.prayer.app.appcomponents.base.BaseActivity
+import com.prayer.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
---
+++ leen_s_application2/app/src/test/java/com/leensapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.leensapplication.app
+
+import com.leensapplication.app.appcomponents.base.BaseActivity
+import com.leensapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
