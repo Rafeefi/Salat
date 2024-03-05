a4594d9250606c76983bdcc55a2b88d5958976f9
LeenAlshehri2003 <miss.leen.2704@gmail.com>
2024-03-05 12:48:48 +0300

    Leen
    Leen

--- app/src/main/java/com/prayer/app/Privacy.java
+++ app/src/main/java/com/prayer/app/Privacy.java
@@ -9,6 +9,6 @@ public class Privacy extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_privacy);
+        setContentView(R.layout.time_calculations);
     }
 }
