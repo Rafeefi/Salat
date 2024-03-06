8310b3fdad374fc67cd0842b5dead03bd5558a0e
lamaa5 <157628304+lamaa5@users.noreply.github.com>
2024-03-06 17:12:37 +0300

    lama

--- app/src/main/java/com/prayer/app/appcomponents/ui/silent.java
+++ app/src/main/java/com/prayer/app/appcomponents/ui/silent.java
@@ -1,14 +1,83 @@
 package com.prayer.app.appcomponents.ui;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.prayer.app.R;
+
+
+
 public class silent extends AppCompatActivity {
+
+    Button startB,endB;
+    TextView startText,endText;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.silentpage);
+        startB=findViewById(R.id.StarttimeB);
+        endB=findViewById(R.id.EndtimeB);
+        startText=findViewById(R.id.StarttextView);
+        endText=findViewById(R.id.EndtextView2);
+
+
+        startB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog dialog=new TimePickerDialog(silent.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hours, int minutes) {
+
+                    endText.setText(String.valueOf(hours)+":"+String.valueOf(minutes));
+
+                }
+
+
+            }, 15, 50, true);
+
+
+
+            }
+
+        });
+
+
+
+
+
+        endB .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               TimePickerDialog dialog=new TimePickerDialog(silent.this, new TimePickerDialog.OnTimeSetListener() {
+                   @Override
+                   public void onTimeSet(TimePicker view, int hours, int minutes) {
+
+                       endText.setText(String.valueOf(hours)+":"+String.valueOf(minutes));
+
+                   }
+
+
+               }, 15, 50, true);
+
+
+
+                   }
+
+    });
     }
+
+
 }
+
+
+
+
