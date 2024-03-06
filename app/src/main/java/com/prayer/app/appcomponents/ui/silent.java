79eb38ba83db78341a0c799a1d18dced6e3d7d62
Danah970 <danah.9700@gmail.com>
2024-03-06 16:28:16 +0300

    edit

---
+++ app/src/main/java/com/prayer/app/appcomponents/ui/silent.java
@@ -0,0 +1,15 @@
+package com.prayer.app.appcomponents.ui;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.prayer.app.R;
+
+public class silent extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.silentpage);
+    }
+}
