9b257154f2d78d63b951b83328f51c9a262c0645
lamaa5 <157628304+lamaa5@users.noreply.github.com>
2024-03-06 17:14:08 +0300

    lama

--- app/src/main/java/com/prayer/app/appcomponents/ui/silent.java
+++ app/src/main/java/com/prayer/app/appcomponents/ui/silent.java
@@ -6,10 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.prayer.app.R;
 
 
