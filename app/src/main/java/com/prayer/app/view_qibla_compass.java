8cf77c119eb06fc1c9c89fea30e79b16b0188286
LeenAlshehri2003 <miss.leen.2704@gmail.com>
2024-03-15 16:05:52 +0300

    Leen Edits

---
+++ app/src/main/java/com/prayer/app/view_qibla_compass.java
@@ -0,0 +1,19 @@
+package com.prayer.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class view_qibla_compass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_qibla_compass);
+
+    }
+}
